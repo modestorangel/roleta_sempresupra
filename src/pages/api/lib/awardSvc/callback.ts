fc76761930ac3711423b2cce5d094c5ed7fdf705
Modesto Rangel <victor.web.rangel@gmail.com>
2023-08-14 10:31:39 -0300

    Update callback.ts

--- src/pages/api/lib/awardSvc/callback.ts
+++ src/pages/api/lib/awardSvc/callback.ts
@@ -1,12 +1,12 @@
 const awardsCallback = [
-    { description: 'R$ 30 no iFood' },
-    { description: 'R$ 50 na Americanas' },
-    { description: 'isenção boleto' },
-    { description: 'R$ 10 OFF no boleto' },
-    { description: 'R$ 20 OFF no boleto' },
-    { description: 'R$ 30 no iFood' },
-    { description: 'R$ 50 na Americanas' },
-    { description: 'isenção boleto' },
+    { description: 'R$ 50 no iFood' },
+    { description: 'R$ 50 de Desconto' },
+    { description: '50% de Desconto' },
+    { description: 'R$ 75 no Outback' },
+    { description: 'R$ 25 de Desconto' },
+    { description: 'R$ 30 de Desconto' },
+    { description: 'R$ 100 na Americanas' },
+    { description: '100% de Desconto' },
 ]
 
 export default awardsCallback
