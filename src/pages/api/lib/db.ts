bc5c09b95a008417ab3a98f981e506a3654d4c04
Modesto Rangel <victor.web.rangel@gmail.com>
2023-08-14 09:25:02 -0300

    Update db.ts

--- src/pages/api/lib/db.ts
+++ src/pages/api/lib/db.ts
@@ -3,7 +3,7 @@ import mysql from 'serverless-mysql';
 const db = mysql({
     config: {
         host: "108.181.92.69",
-        port: 3306,
+        port: "3306",
         database: "roleta_ss",
         user: "admin_roleta",
         password: "v6ECM@TJZZk18gVG"
